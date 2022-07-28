d3712d5d5e93a9b068d8803104c4f9a90a65eacc
Apuu-Khurud1711999 <khurudapurva@gmail.com>
2022-07-28 14:08:49 +0530

    Initial Project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto mx-w-6xl text-center drop-shadow text-gray-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
